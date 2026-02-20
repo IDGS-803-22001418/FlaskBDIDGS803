2f9fe7957cf5972436178d6493f92c8ce3645710
IDGS-803-22001418 <melchor.ru.go@gmail.com>
2026-02-19 21:09:14 -0600

    19/02/2026 - Detalles

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import forms
 from models import db, Alumnos
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'una-clave-secreta-muy-dificil-de-adivinar'
 app.config.from_object(DevelopmentConfig)
 csrf = CSRFProtect()
 
@@ -14,14 +15,31 @@ csrf = CSRFProtect()
 def page_not_found(e): # type: ignore
 	return render_template("404.html")
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 @app.route("/index")
 def index():
-	return render_template("index.html")
+	alumnos = Alumnos.query.all() # type: ignore
+	return render_template("index.html", alumnos=alumnos)
 
-@app.route("/Alumnos")
+@app.route("/detalle")
+def detalle():
+	id = request.args.get("id")
+	alumno = db.session.query(Alumnos).filter(Alumnos.id==id).first()
+	return render_template("detalles.html", alumno=alumno)
+
+@app.route("/Alumnos", methods=["GET", "POST"])
 def alumnos():
-	return render_template("Alumnos.html")
+	create_form = forms.UserForm(request.form)
+	if request.method == "POST":
+		alumno = Alumnos(
+			nombre=create_form.nombre.data,  # type: ignore
+			apaterno=create_form.apaterno.data,  # type: ignore
+			email=create_form.email.data  # type: ignore
+		)
+		db.session.add(alumno)
+		db.session.commit()
+		return redirect(url_for('index'))
+	return render_template("Alumnos.html", form=create_form)
 
 if __name__ == '__main__':
 	csrf.init_app(app) # type: ignore
