93bb9b7c1c98ae9c29c763e2f7e8c6633f42ee44
IDGS-803-22001418 <melchor.ru.go@gmail.com>
2026-02-26 20:14:39 -0600

    26/02/2026 - ABC completo

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, g
+from flask import Flask, render_template, request, redirect, url_for
 from flask_wtf.csrf import CSRFProtect # type: ignore
 from config import DevelopmentConfig
 
@@ -21,12 +21,6 @@ def index():
 	alumnos = Alumnos.query.all() # type: ignore
 	return render_template("index.html", alumnos=alumnos)
 
-@app.route("/detalle")
-def detalle():
-	id = request.args.get("id")
-	alumno = db.session.query(Alumnos).filter(Alumnos.id==id).first()
-	return render_template("detalles.html", alumno=alumno)
-
 @app.route("/Alumnos", methods=["GET", "POST"])
 def alumnos():
 	create_form = forms.UserForm(request.form)
@@ -40,6 +34,47 @@ def alumnos():
 		db.session.commit()
 		return redirect(url_for('index'))
 	return render_template("Alumnos.html", form=create_form)
+
+@app.route("/detalle")
+def detalle():
+	id = request.args.get("id")
+	alumno = db.session.query(Alumnos).filter(Alumnos.id==id).first()
+	return render_template("detalles.html", alumno=alumno)
+
+@app.route("/modificar", methods=["GET", "POST"])
+def modificar():
+	create_form = forms.UserForm(request.form)
+	id = request.args.get("id")
+	alumno = db.session.query(Alumnos).filter(Alumnos.id==id).first()
+	if request.method == "GET":
+		create_form.nombre.data = alumno.nombre # type: ignore
+		create_form.apaterno.data = alumno.apaterno # type: ignore
+		create_form.email.data = alumno.email # type: ignore
+	if request.method == "POST":
+		alumno.nombre = create_form.nombre.data # type: ignore
+		alumno.apaterno = create_form.apaterno.data # type: ignore
+		alumno.email = create_form.email.data  # type: ignore
+		db.session.add(alumno)
+		db.session.commit()
+		return redirect(url_for('index'))
+	return render_template("modificar.html", form=create_form)
+
+@app.route("/eliminar", methods=["GET", "POST"])
+def eliminar():
+	create_form = forms.UserForm(request.form)
+	id = request.args.get("id")
+	alumno = db.session.query(Alumnos).filter(Alumnos.id==id).first()
+	if request.method == "GET":
+		create_form.nombre.data = alumno.nombre # type: ignore
+		create_form.apaterno.data = alumno.apaterno # type: ignore
+		create_form.email.data = alumno.email # type: ignore
+	if request.method == "POST":
+		db.session.delete(alumno)
+		db.session.commit()
+		return redirect(url_for('index'))
+	return render_template("eliminar.html", form=create_form)
+
+
 
 if __name__ == '__main__':
 	csrf.init_app(app) # type: ignore
